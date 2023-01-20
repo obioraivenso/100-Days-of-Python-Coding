6e875830df32b7b12f655d134543563aa4c2ef7d
obioraivenso <>
2023-01-20 22:50:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hello And Welcome To The BAND NAME GENERATOR!")
+city_name = input("What is the name of your City?\n")
+pet_name = input("What is the name of your Pet?\n")
+print("Your band name should be " + city_name + " " + pet_name)
